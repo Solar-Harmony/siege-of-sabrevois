96faf93f8d9e05eb4b5b1bda9d4f3daa6ebb88fe
Thurinum <m.gagnon941@gmail.com>
2026-04-11 11:25:24 -0400

    feat: Idk

---
+++ Assets/Sabrevois/Gameplay/Dialogue/ConversationTopic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sabrevois.AI.Considerations;
+using UnityEngine;
+
+namespace Sabrevois.Gameplay.Dialogue
+{
+    [Serializable]
+    public record ConversationTopic
+    {
+        [SerializeField]
+        public string[] Responses;
+        
+        [SerializeField]
+        public string[] FollowUps;
+
+        [SerializeReference] 
+        public IConsiderationSource Condition;
+    }
+}
