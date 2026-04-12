8eea88c4bcd609aa6ecae27621befa860a8a6a16
Yanick Duhaime <YanickDuhaime1@gmail.com>
2026-04-12 17:59:32 -0400

    feat: Ajouté des [Serializable]

--- Assets/Sabrevois/Gameplay/AI/DataSources/HungerPercentageSource.cs
+++ Assets/Sabrevois/Gameplay/AI/DataSources/HungerPercentageSource.cs
@@ -1,9 +1,11 @@
-﻿using Sabrevois.AI.Considerations;
+﻿using System;
+using Sabrevois.AI.Considerations;
 using Sabrevois.Utils;
 using UnityEngine;
 
 namespace Sabrevois.Gameplay.AI.DataSources
 {
+    [Serializable]
     public class HungerPercentageSource : IConsiderationSource
     {
         public float GetValue(GameObject agent)
