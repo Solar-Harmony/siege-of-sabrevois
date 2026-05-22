333ac6545671c32f2fd25d648eb8cb98a5624c1b
Thurinum <m.gagnon941@gmail.com>
2026-05-21 23:50:24 -0400

    feat: Realtime probe reflections around player

---
+++ Assets/Sabrevois/Level/Reflections/RealtimeReflectionProbe.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Sabrevois.Level.Reflections
+{
+    public class RealtimeReflectionProbe : MonoBehaviour
+    {
+        private static readonly int PlayerCubemapId = Shader.PropertyToID("_PlayerCubemap");
+        
+        public ReflectionProbe Probe;
+        
+        [Min(0)]
+        public float UpdateInterval = 1.0f;
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(UpdateCubemap), 0f, UpdateInterval);
+        }
+
+        private void UpdateCubemap()
+        {
+            Probe.RenderProbe();
+            Cubemap cube = Probe.texture as Cubemap;
+            Shader.SetGlobalTexture(PlayerCubemapId, cube);
+        }
+    }
+}
